a8e23f2ce645c3b4230d6e8e26131020cd45baa3
Noel Delgado <noeldelgadom@myhost.local>
2017-08-24 09:46:41 -0500

    last commit

---
+++ app/mailers/proposal_mailer.rb
@@ -0,0 +1,7 @@
+class ProposalMailer < ApplicationMailer
+  def email(proposal)
+    @proposal = proposal
+    mail( to: @proposal.client_email,
+          subject: "You've received a new proposal",
+        )
+end
